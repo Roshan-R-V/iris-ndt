103436e9fdec14d3050be73b36ea6c0c30b0a106
Roshan-R-V <roshanvivek3920@gmail.com>
2025-03-10 19:44:19 +0000

    fix : Modified Contact form to send email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,19 +5,22 @@ const Contact = () => (
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <div className="card p-4 shadow-lg bg-dark text-white border-secondary">
-                        <form className="mt-3">
+                        <form action="https://formsubmit.co/roshanvivek2000@gmail.com" method="POST" className="mt-3">
+                            <input type="hidden" name="_subject" value="New Contact Form Submission"/>
                             <div className="mb-3">
                                 <p>Name</p>
-                                <input type="text" className="form-control bg-dark text-white border-secondary" placeholder="Your Name" required />
+                                <input type="text" className="form-control bg-dark text-white border-secondary"
+                                       name="name" placeholder="Your Name" required/>
                             </div>
                             <div className="mb-3">
                                 <p>Email</p>
                                 <input type="email" className="form-control bg-dark text-white border-secondary"
-                                       placeholder="Your Email" required/>
+                                       name="email" placeholder="Your Email" required/>
                             </div>
                             <div className="mb-3">
                                 <p>Message</p>
-                                <textarea className="form-control bg-dark text-white border-secondary" rows="3" placeholder="Your Message" required></textarea>
+                                <textarea className="form-control bg-dark text-white border-secondary" rows="3"
+                                          name="message" placeholder="Your Message" required></textarea>
                             </div>
                             <div className="text-center">
                                 <button className="btn btn-primary" type="submit">Submit</button>
